2101a7aff0e502c246e141db9d0258f43617ba5c
bala-amypo <bala@amypo.in>
2025-12-18 04:32:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PatientProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PatientProfileServiceImpl.java
@@ -1,33 +1,11 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.example.demo.model.PatientProfile;
-import com.example.demo.repository.PatientProfileRepository;
-import com.example.demo.service.PatientProfileService;
 
-@Service
-public class PatientProfileServiceImpl implements PatientProfileService {
-    @Autowired
-    PatientProfileRepository patientProfileRepository;
-
-    @Override
-    public PatientProfile createPatient(PatientProfile patientProfile) {
-        return patientProfileRepository.save(patientProfile);
-    }
-     
-    @Override
-    public List<PatientProfile> getPatientProfile(){
-        return patientProfileRepository.findAll();
-    }
-
-    @Override
-    public PatientProfile getById(long id){
-        Optional<PatientProfile> optionalProfile = PatientProfileRepository.findByid(id);
-        return optionalProfile.orElse(null);
-    }
+public interface PatientProfileService {
+    PatientProfile createPatient(PatientProfile patientProfile);
+    List<PatientProfile> getPatientProfile();
+     PatientProfile getById(long id);
 }
