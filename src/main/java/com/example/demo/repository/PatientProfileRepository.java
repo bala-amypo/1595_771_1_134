23e2ff9801aaf74d4e52032058b7bcda35c9f277
bala-amypo <bala@amypo.in>
2025-12-19 06:12:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/PatientProfileRepository.java
+++ src/main/java/com/example/demo/repository/PatientProfileRepository.java
@@ -1,19 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.PatientProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.model.PatientProfile;
-
-@Repository
 public interface PatientProfileRepository extends JpaRepository<PatientProfile, Long> {
 
     Optional<PatientProfile> findByEmail(String email);
 
     Optional<PatientProfile> findByPatientId(String patientId);
-    Optional<PatientProfile> findByPatientId(String patientId);
-    Optional<PatientProfile> updatePatientStatus(Long id, boolean active);
-
 }
