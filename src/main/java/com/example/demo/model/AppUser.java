18d4190202061d954f4e361f5826e229959cd14a
bala-amypo <bala@amypo.in>
2025-12-22 05:18:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -0,0 +1,76 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "app_users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    // Required by JPA
+    public AppUser() {
+    }
+
+    public AppUser(Long id, String email, String password,
+                   String fullName, UserRole role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}
